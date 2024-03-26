a7b6530493bed333117d57cf69fbb671d728228c
TStar111 <rickyg@andrew.cmu.edu>
2024-03-25 21:35:18 -0400

    Tested detect_sign with heur and speaker

--- scripts/detect_sign.py
+++ scripts/detect_sign.py
@@ -28,13 +28,15 @@ engine = pyttsx3.init()
 dict_to_num = {chr(i): i - 97 for i in range(97, 107)}
 dict_to_let = {0:"a", 1:"b", 2:"c", 3:"d", 4:"e", 5:"f", 6:"g", 7:"h", 8:"i", 9:"k", 10:" "}
 
-# Empty storage
+# Empty storage (aka Global variables)
 start_time = None
 end_time = None
 class_tracker = [None, None]
 
 # Function that will return [bool, letter, new_tracker]
 def classification_heuristic(new_letter, tracker, consecutive):
+    global start_time
+    global end_time
     curr_letter = tracker[0]
     if new_letter == curr_letter:
         tracker[1] += 1
@@ -52,6 +54,7 @@ def classification_heuristic(new_letter, tracker, consecutive):
         return False, None, tracker
 
 async def collect_bt(address, model):
+    global class_tracker
     client = BleakClient(address)
     await client.connect()
     if client.is_connected():
@@ -63,10 +66,10 @@ async def collect_bt(address, model):
         received_data = bytes_to_floats(value)
         data_array = torch.tensor(received_data)  # Split and convert to int array
         
-        start = time.time()
+        # start = time.time()
         probs = model(data_array)
-        end = time.time()
-        print("ML time: ", end-start, "sec")
+        # end = time.time()
+        # print("ML time: ", end-start, "sec")
 
         # Get the index of the highest value
         index = torch.argmax(probs, dim=0).item()
@@ -83,13 +86,13 @@ async def collect_bt(address, model):
 def handle_usb_data(model, ser):
     if ser.in_waiting > 0:  # Check if there's data available to read
         received_data = ser.readline().decode().strip()
-        print(received_data)
+        # print(received_data)
         data_array = torch.tensor([float(x) for x in received_data.split(',')])  # Split and convert to int array
         
         start = time.time()
         probs = model(data_array)
         end = time.time()
-        print("ML time: ", end-start, "sec")
+        # print("ML time: ", end-start, "sec")
 
         # Get the index of the highest value
         index = torch.argmax(probs, dim=0).item()
@@ -129,15 +132,18 @@ def main(connection_type, device_mac_address):
 
             # Keep reading data
             while True:
-                time.sleep(0.1)
+                time.sleep(0.05)
                 yhat = handle_usb_data(model, ser)
+                # print(yhat)
                 
-                passed, letter, class_tracker = classification_heuristic(yhat, class_tracker, consecutive)
-                if passed:
-                    print(letter)
-                    print("Elapsed time:", end_time - start_time)
-                    engine.say(letter)
-                    engine.runAndWait() 
+                global class_tracker
+                if yhat is not None:
+                    passed, letter, class_tracker = classification_heuristic(yhat, class_tracker, consecutive)
+                    if passed:
+                        print(letter)
+                        print("Elapsed time:", end_time - start_time)
+                        engine.say(letter)
+                        engine.runAndWait() 
 
         except serial.SerialException as e:
             print("Serial Error:", e)
