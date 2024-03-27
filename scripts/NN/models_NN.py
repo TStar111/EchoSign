76cf59328a3b33e51f3fee6a4816e10b808888f1
TStar111 <rickyg@andrew.cmu.edu>
2024-03-26 21:23:29 -0400

    Added model, added data_collect

--- scripts/NN/models_NN.py
+++ scripts/NN/models_NN.py
@@ -11,3 +11,16 @@ class SimpleNN(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
+    
+class SimpleNN2(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(SimpleNN, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
