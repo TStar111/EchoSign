95b92534946695d72a98f89fe89feee151f80a04
TStar111 <rickyg@andrew.cmu.edu>
2024-03-28 17:39:49 -0400

    Restructured/split up bt/usb

--- scripts/detect_sign_usb.py
+++ scripts/detect_sign_usb.py
@@ -1,28 +1,26 @@
-import torch
 import time
-import pyttsx3
+import serial
+import torch
+
+from NN.models_NN import SimpleNN
 
 from utils import bytes_to_floats, initialize_bt
-from NN.models_NN import SimpleNN
+
+# Parameters
+input_dim = 14
+hidden_dim = 64
+output_dim = 11
+checkpoint_path = '../models/simpleNN_none1.pt'
+consecutive = 8
+
+# Alphabet dictionary
+dict_to_num = {chr(i): i - 97 for i in range(97, 107)}
+dict_to_let = {0:"a", 1:"b", 2:"c", 3:"d", 4:"e", 5:"f", 6:"g", 7:"h", 8:"i", 9:"k", 10:" "}
 
 # Empty storage (aka Global variables)
 start_time = None
 end_time = None
 class_tracker = [None, None]
-dict_to_let = {0:"a", 1:"b", 2:"c", 3:"d", 4:"e", 5:"f", 6:"g", 7:"h", 8:"i", 9:"k", 10:" "}
-
-# Model parameters
-input_dim = 14
-hidden_dim = 64
-output_dim = 11
-checkpoint_path = '../models/simpleNN_none1.pt'
-
-# Hyperparameters
-consecutive = 8
-
-# ARDUINO Bluetooth information
-CHARACTERISTIC_UUID = "19B10001-E8F2-537E-4F6C-D104768A1214"
-address = "02:81:b7:4b:04:26" # MAC addres of the remove ble device
 
 # Function that will return [bool, letter, new_tracker]
 def classification_heuristic(new_letter, tracker, consecutive):
@@ -44,11 +42,24 @@ def classification_heuristic(new_letter, tracker, consecutive):
         start_time = time.time()
         return False, None, tracker
 
+def handle_usb_data(model, ser):
+    if ser.in_waiting > 0:  # Check if there's data available to read
+        received_data = ser.readline().decode().strip()
+        data_array = torch.tensor([float(x) for x in received_data.split(',')])  # Split and convert to int array
+        
+        probs = model(data_array)
+
+        # Get the index of the highest value
+        index = torch.argmax(probs, dim=0).item()
+
+        yhat = dict_to_let[index]
+
+        ser.reset_input_buffer()
+        return yhat
+    else:
+        return None
+
 if __name__ == "__main__":
-    peripheral, service_uuid, characteristic_uuid = initialize_bt()
-
-    # engine = pyttsx3.init()
-
     # Initialize model with saved weights
     model = SimpleNN(input_dim, hidden_dim, output_dim)
 
@@ -62,30 +73,34 @@ if __name__ == "__main__":
     model.eval()
 
     try:
+        # Connect to Arduino via USB
+        ser = serial.Serial('COM9', 9600, timeout=1)  # Adjust port and baud rate as needed
+
         # Keep reading data
         while True:
             time.sleep(0.05)
-            contents = bytes_to_floats(peripheral.read(service_uuid, characteristic_uuid))
-            data_array = torch.tensor(contents)
-            probs = model(data_array)
-            index = torch.argmax(probs, dim=0).item()
-            yhat = dict_to_let[index]
-                        
+            yhat = handle_usb_data(model, ser)
+            # print(yhat)
+            
             if yhat is not None:
                 passed, letter, class_tracker = classification_heuristic(yhat, class_tracker, consecutive)
                 if passed:
                     print(letter)
                     print("Elapsed time:", end_time - start_time)
-                    # engine.say(letter)
-                    # engine.runAndWait() 
 
+    except serial.SerialException as e:
+        print("Serial Error:", e)
+        ser.close()  # Close the serial connection before exiting
+        print("Serial connection closed.")
+        exit()
 
     except KeyboardInterrupt:
         print("KeyboardInterrupt: Stopping inference...")
-        peripheral.disconnect()
+        ser.close()  # Close the serial connection before exiting
+        print("Serial connection closed.")
         exit()
 
 # Run command below in scripts folder
-# python detect_sign_bt.py
+# python detect_sign_usb.py
         
 # Make sure to select the proper ML model before starting
--- scripts/test_BT.py
+++ scripts/test_BT.py
@@ -1,72 +1,59 @@
-# import asyncio
-# from bleak import BleakClient
+import simplepyble
 
-# ADDRESS = "02:81:b7:4b:04:26"  # Replace with the address of your Arduino Nano BLE
-# SERVICE_UUID = "12345678-1234-1234-1234-123456789ABC"
-# CHARACTERISTIC_UUID = "12345678-1234-1234-1234-123456789ABC"
+from utils import bytes_to_floats
 
-# async def receive_data(address):
-#     async with BleakClient(address) as client:
-#         print("Before connect")
-#         try:
-#             await client.connect()
-#         except Exception as e:
-#             print("Error:", e)
-#         print("After connect")
-#         value = await client.read_gatt_char(CHARACTERISTIC_UUID)
-#         received_data = value.decode('utf-8')  # Decode bytes to string
-#         print("Received:", received_data)
-#         float_values = [float(x) for x in received_data.split(',')]
-#         print("Float values:", float_values)
+if __name__ == "__main__":
+    adapters = simplepyble.Adapter.get_adapters()
 
-# async def main():
-#     await receive_data(ADDRESS)
+    if len(adapters) == 0:
+        print("No adapters found")
 
-# if __name__ == "__main__":
-#     asyncio.run(main())
+    # Query the user to pick an adapter
+    print("Please select an adapter:")
+    for i, adapter in enumerate(adapters):
+        print(f"{i}: {adapter.identifier()} [{adapter.address()}]")
 
-import asyncio
-import sys
-import time
+    choice = int(input("Enter choice: "))
+    adapter = adapters[choice]
 
-from bleak import *
-#from bleak import BleakScanner, BleakClient
-#from bleak.backends.scanner import AdvertisementData
-import struct
+    print(f"Selected adapter: {adapter.identifier()} [{adapter.address()}]")
 
-# Define a function to decode a byte array to multiple floats
-def bytes_to_floats(byte_data):
-    # Assuming byte_data is a bytes object
-    # 'f' format specifier denotes a single precision float (4 bytes)
-    # Calculate the number of floats by dividing the total length of byte_data by 4
-    num_floats = len(byte_data) // 4
-    # Use struct.unpack to convert bytes to floats
-    # 'f' format specifier denotes a single precision float (4 bytes)
-    floats = struct.unpack('<{}f'.format(num_floats), byte_data)
-    return floats
+    adapter.set_callback_on_scan_start(lambda: print("Scan started."))
+    adapter.set_callback_on_scan_stop(lambda: print("Scan complete."))
+    # adapter.set_callback_on_scan_found(lambda peripheral: print(f"Found {peripheral.identifier()} [{peripheral.address()}]"))
 
-# Example usage:
-# byte_data = b'\x00\x00\xa0\x42\x00\x00\x00\x43\x00\x00\x80\x44\x00\x00\xa0\x44\x00\x00\x00\x45'  # Example byte array
-# decoded_floats = bytes_to_floats(byte_data)
-# print(decoded_floats)  # Output: (100.0, 103.0, 128.0, 160.0, 165.0)
+    # Scan for 1 seconds
+    adapter.scan_for(1000)
+    peripherals = adapter.scan_get_results()
 
+    # Query the user to pick a peripheral
+    print("Please select a peripheral:")
+    for i, peripheral in enumerate(peripherals):
+        print(f"{i}: {peripheral.identifier()} [{peripheral.address()}]")
 
+    choice = int(input("Enter choice: "))
+    peripheral = peripherals[choice]
 
-CHARACTERISTIC_UUID = "19B10001-E8F2-537E-4F6C-D104768A1214"
-address = "02:81:b7:4b:04:26" # MAC addres of the remove ble device
+    print(f"Connecting to: {peripheral.identifier()} [{peripheral.address()}]")
+    peripheral.connect()
 
-async def main(address):
-    client = BleakClient(address)
-    try:
-        await client.connect()
-        if client.is_connected():
-            print('I\'m connected to the device ! ')
+    print("Successfully connected, listing services...")
+    services = peripheral.services()
+    service_characteristic_pair = []
+    for service in services:
+        for characteristic in service.characteristics():
+            service_characteristic_pair.append((service.uuid(), characteristic.uuid()))
 
-        while True:
-            value = await client.read_gatt_char(CHARACTERISTIC_UUID)
-            received_data = bytes_to_floats(value)
-            print("Received:", received_data)
-    except Exception as e:
-        print(e)
+    # Query the user to pick a service/characteristic pair
+    print("Please select a service/characteristic pair:")
+    for i, (service_uuid, characteristic) in enumerate(service_characteristic_pair):
+        print(f"{i}: {service_uuid} {characteristic}")
 
-asyncio.run(main(address))
+    choice = int(input("Enter choice: "))
+    service_uuid, characteristic_uuid = service_characteristic_pair[choice]
+
+    # Write the content to the characteristic
+    contents = bytes_to_floats(peripheral.read(service_uuid, characteristic_uuid))
+    print(f"Contents: {contents}")
+
+    peripheral.disconnect()
